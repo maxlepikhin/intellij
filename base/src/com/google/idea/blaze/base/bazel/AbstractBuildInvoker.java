brian-mcnamara/intellij-bazel

We currently support user blazercs only if they are present in the workspace root. This CL prompts the user if they want to copy .blazerc from $HOME to $WORKSPACE if the .blazerc is detected at the home directory and not at the workspace root.
PiperOrigin-RevId: 462166067

base/src/com/google/idea/blaze/base/bazel/AbstractBuildInvoker.java
@@ -148,4 +148,9 @@ public abstract class AbstractBuildInvoker implements BuildInvoker {
             WorkspaceRoot.fromProject(project),
             syncFlags);
   }
+
+  @Override
+  public boolean supportsHomeBlazerc() {
+    return true;
+  }
 }

base/src/com/google/idea/blaze/base/bazel/BuildSystem.java
@@ -74,6 +74,9 @@ public interface BuildSystem {
 
     /** Returns a {@link BlazeCommandRunner} to be used to invoke the build. */
     BlazeCommandRunner getCommandRunner();
+
+    /** Indicates whether the invoker supports user .blazerc from home directories. */
+    boolean supportsHomeBlazerc();
   }
 
   /** Returns the type of the build system. */

base/src/com/google/idea/blaze/base/sync/BuildPhaseSyncTask.java
@@ -26,6 +26,7 @@ import com.google.idea.blaze.base.bazel.BuildSystem.BuildInvoker;
 import com.google.idea.blaze.base.bazel.BuildSystem.SyncStrategy;
 import com.google.idea.blaze.base.command.BlazeInvocationContext;
 import com.google.idea.blaze.base.command.BlazeInvocationContext.ContextType;
+import com.google.idea.blaze.base.command.BlazercMigrator;
 import com.google.idea.blaze.base.dependencies.BlazeQuerySourceToTargetProvider;
 import com.google.idea.blaze.base.dependencies.TargetInfo;
 import com.google.idea.blaze.base.io.FileOperationProvider;
@@ -62,6 +63,7 @@ import com.google.idea.blaze.base.sync.sharding.ShardedTargetList;
 import com.google.idea.blaze.base.sync.sharding.SuggestBuildShardingNotification;
 import com.google.idea.blaze.base.sync.workspace.WorkingSet;
 import com.google.idea.common.experiments.BoolExperiment;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import java.io.File;
@@ -227,12 +229,12 @@ final class BuildPhaseSyncTask {
       printShardingSummary(context, targetCount, shardedTargets.shardCount(), parallel);
     }
 
-    BuildInvoker syncBuildInvoker = null;
-    if (parallel) {
-      syncBuildInvoker = buildSystem.getParallelBuildInvoker(project, context).orElse(null);
-    }
-    if (syncBuildInvoker == null) {
-      syncBuildInvoker = defaultInvoker;
+    BuildInvoker syncBuildInvoker =
+        buildSystem.getParallelBuildInvoker(project, context).orElse(defaultInvoker);
+    if (!syncBuildInvoker.supportsHomeBlazerc() && BlazercMigrator.needMigration(project)) {
+      context.output(
+          SummaryOutput.output(Prefix.INFO, "No .blazerc found at workspace root!").log().dedupe());
+      ApplicationManager.getApplication().invokeLater(() -> migrateBlazerc(context));
     }
     resultBuilder.setBlazeInfo(syncBuildInvoker.getBlazeInfo());
 
@@ -423,5 +425,25 @@ final class BuildPhaseSyncTask {
               ImmutableSet.of(OutputGroup.RESOLVE, OutputGroup.INFO),
               BlazeInvocationContext.SYNC_CONTEXT);
         });
+  }
+
+  private void migrateBlazerc(BlazeContext context) {
+    if (!BlazercMigrator.promptMigration(project)) {
+      return;
+    }
+
+    if (BlazercMigrator.copyBlazercToWorkspace(project)) {
+      context.output(
+          SummaryOutput.output(Prefix.INFO, "Copied .blazerc from home to workspace root")
+              .log()
+              .dedupe());
+    } else {
+      context.output(
+          SummaryOutput.output(
+                  Prefix.INFO,
+                  "Error copying .blazerc from home to workspace root; syncing without one.")
+              .log()
+              .dedupe());
+    }
   }
 }

maxlepikhin/intellij

base/src/com/google/idea/blaze/base/bazel/AbstractBuildInvoker.java
@@ -148,4 +148,9 @@ public abstract class AbstractBuildInvoker implements BuildInvoker {
             WorkspaceRoot.fromProject(project),
             syncFlags);
   }
+
+  @Override
+  public boolean getSupportsHomeBlazerc() {
+    return true;
+  }
 }

base/src/com/google/idea/blaze/base/bazel/BuildSystem.java
@@ -74,6 +74,9 @@ public interface BuildSystem {
 
     /** Returns a {@link BlazeCommandRunner} to be used to invoke the build. */
     BlazeCommandRunner getCommandRunner();
+
+    /** Indicates whether the invoker supports user .blazerc from home directories. */
+    boolean getSupportsHomeBlazerc();
   }
 
   /** Returns the type of the build system. */

base/src/com/google/idea/blaze/base/sync/BuildPhaseSyncTask.java
@@ -26,6 +26,7 @@ import com.google.idea.blaze.base.bazel.BuildSystem.BuildInvoker;
 import com.google.idea.blaze.base.bazel.BuildSystem.SyncStrategy;
 import com.google.idea.blaze.base.command.BlazeInvocationContext;
 import com.google.idea.blaze.base.command.BlazeInvocationContext.ContextType;
+import com.google.idea.blaze.base.command.BlazercMigrator;
 import com.google.idea.blaze.base.dependencies.BlazeQuerySourceToTargetProvider;
 import com.google.idea.blaze.base.dependencies.TargetInfo;
 import com.google.idea.blaze.base.io.FileOperationProvider;
@@ -62,6 +63,7 @@ import com.google.idea.blaze.base.sync.sharding.ShardedTargetList;
 import com.google.idea.blaze.base.sync.sharding.SuggestBuildShardingNotification;
 import com.google.idea.blaze.base.sync.workspace.WorkingSet;
 import com.google.idea.common.experiments.BoolExperiment;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import java.io.File;
@@ -227,12 +229,12 @@ final class BuildPhaseSyncTask {
       printShardingSummary(context, targetCount, shardedTargets.shardCount(), parallel);
     }
 
-    BuildInvoker syncBuildInvoker = null;
-    if (parallel) {
-      syncBuildInvoker = buildSystem.getParallelBuildInvoker(project, context).orElse(null);
-    }
-    if (syncBuildInvoker == null) {
-      syncBuildInvoker = defaultInvoker;
+    BuildInvoker syncBuildInvoker =
+        buildSystem.getParallelBuildInvoker(project, context).orElse(defaultInvoker);
+    if (!syncBuildInvoker.getSupportsHomeBlazerc() && BlazercMigrator.needMigration(project)) {
+      context.output(
+          SummaryOutput.output(Prefix.INFO, "No .blazerc found at workspace root!").log().dedupe());
+      ApplicationManager.getApplication().invokeLater(() -> migrateBlazerc(context));
     }
     resultBuilder.setBlazeInfo(syncBuildInvoker.getBlazeInfo());
 
@@ -423,5 +425,25 @@ final class BuildPhaseSyncTask {
               ImmutableSet.of(OutputGroup.RESOLVE, OutputGroup.INFO),
               BlazeInvocationContext.SYNC_CONTEXT);
         });
+  }
+
+  private void migrateBlazerc(BlazeContext context) {
+    if (!BlazercMigrator.promptMigration(project)) {
+      return;
+    }
+
+    if (BlazercMigrator.copyBlazercToWorkspace(project)) {
+      context.output(
+          SummaryOutput.output(Prefix.INFO, "Copied .blazerc from home to workspace root")
+              .log()
+              .dedupe());
+    } else {
+      context.output(
+          SummaryOutput.output(
+                  Prefix.INFO,
+                  "Error copying .blazerc from home to workspace root; syncing without one.")
+              .log()
+              .dedupe());
+    }
   }
 }